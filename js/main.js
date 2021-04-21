68668c8b5ae84e8b3cb84f5096ec457a4839e6d4
GersonST <gersonsevero90@gmail.com>
2021-04-21 02:22:16 -0300

    aula 1

---
+++ js/main.js
@@ -0,0 +1,10 @@
+var nome = "Gerson Severo";
+var idade = 30;
+var idade2 = 10;
+var frase = "Japão é o melhor time do mundo";
+// alert(nome + " tem " + idade + " anos ");
+// alert(idade + idade2)
+
+console.log(nome)
+console.log(idade*idade2/idade)
+console.log(frase.replace("Japão","Brasil"));
